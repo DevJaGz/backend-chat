8da4048aeac44f937dbfe1f71cc9503245816841
Julian Gomez <jgomez109@unab.edu.co>
2022-09-24 19:59:26 -0500

    Add process env to the config file

--- src/constants/config.constant.js
+++ src/constants/config.constant.js
@@ -1,5 +1,5 @@
 export const config = {
-	BD_URL: 'mongodb+srv://jgomez:%40abc12345@cluster0.omp6p1x.mongodb.net/chat-app-dev',
-	PORT: 3000,
-	PUBLIC_ROUTE: '/'
+	BD_URL: process.env.BD_URL || 'mongodb+srv://jgomez:%40abc12345@cluster0.omp6p1x.mongodb.net/chat-app-dev',
+	PORT: process.env.PORT || 3000,
+	PUBLIC_ROUTE: process.env.PUBLIC_ROUTE || '/'
 };
